ce261a34430e26a4126179af9b9679f6c5215da2
akasymzhaparov <akasymzhaparov>
2025-07-02 14:24:00 +0600

    fix: add prettier and eslint

--- src/icons/Icon.tsx
+++ src/icons/Icon.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TIconType, ICON_TYPES } from '../types';
+import {TIconType, ICON_TYPES} from '../types';
 
 export interface IconProps {
     type: TIconType;
@@ -10,25 +10,26 @@ export interface IconProps {
     style?: React.CSSProperties;
 }
 
-export const Icon: React.FC<IconProps> = ({
-                                              type,
-                                              className,
-                                              width = 24,
-                                              height = 24,
-                                              onClick,
-                                              style
-                                          }) => {
-    return (
-        <svg
-            className={className}
-            width={width}
-            height={height}
-            style={style}
-            onClick={onClick}
-            role={onClick ? "button" : "img"}
-            tabIndex={onClick ? 0 : undefined}
-        >
-            <use href={`#${ICON_TYPES[type]}`} />
-        </svg>
-    );
-};
+export const Icon: React.FC<IconProps> =
+    ({
+         type,
+         className,
+         width = 24,
+         height = 24,
+         onClick,
+         style
+     }) => {
+        return (
+            <svg
+                className={className}
+                width={width}
+                height={height}
+                style={style}
+                onClick={onClick}
+                role={onClick ? "button" : "img"}
+                tabIndex={onClick ? 0 : undefined}
+            >
+                <use href={`#${ICON_TYPES[type]}`}/>
+            </svg>
+        );
+    };
